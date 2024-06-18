3c940328f0c433024d18b7c3161ff78d29989ca9
PabloPaniagua22 <pablomusic57@gmail.com>
2024-06-18 19:03:52 -0300

    Eliminacion de la funcion mostrarInformacion() y agregado de la funcion tarea() y hecho un require_once IEmpleado

--- Empleado.php
+++ Empleado.php
@@ -1,13 +1,11 @@
 <?php
-
+require_once "IEmpleado.php";
 // Definición de la clase Empleado
 abstract class Empleado implements IEmpleado {
-    
     // Declaración de los atributos privados
     private $nombre;
     private $apellido;
     private $sueldo;
-
     // Definición del constructor de la clase Empleado
     public function __construct($nombre, $apellido, $sueldo) {
         // Inicialización de los atributos con los valores proporcionados
@@ -15,31 +13,22 @@ abstract class Empleado implements IEmpleado {
         $this->apellido = $apellido;
         $this->sueldo = $sueldo;
     }
-
     // Método para obtener el valor del atributo $nombre
     public function getNom() {
         return $this->nombre;
     }
-
     // Método para obtener el valor del atributo $apellido
     public function getApellido() {
         return $this->apellido;
     }
-
     // Método para obtener el valor del atributo $sueldo
     public function getSueldo() {
         return $this->sueldo;
     }
-
     // Método para calcular el salario neto aplicando una deducción del 10%
     public function calcularSalarioNeto() {
         return $this->sueldo * 0.9;
     }
-
-    // Método para mostrar la información del empleado
-    public function mostrarInformacion() {
-        // Construcción y retorno de una cadena con la información del empleado
-        return "Nombre: " . $this->nombre . " Apellido: " . $this->apellido . " Sueldo: " . $this->sueldo . " Salario Neto: " . $this->calcularSalarioNeto();
-    }
+    abstract public function tarea();
 }
 ?>
