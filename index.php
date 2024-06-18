a8278c22d5accf72c5ca31c2d209ab04fc9768c7
PabloPaniagua22 <pablomusic57@gmail.com>
2024-06-18 19:07:45 -0300

    Creacion del archivo Index.php

--- index.php
+++ index.php
@@ -1,8 +1,34 @@
+<!DOCTYPE html>
+<html lang="es-ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
 <?php
     require_once "Empleado.php";
     require_once "Diseñador.php";
     require_once "Desarrollador.php";
-    
+    require_once "IEmpleado.php";
 
+        $empleado1 = new Desarrollador("Ana","Perez", 1000, "PHP");
+
+        $empleado2 = new Diseñador("María", "García", 1200, "Photoshop");
+
+        echo "Nombre :" .$empleado1->getNom();
+        echo "<br>";
+        echo "Apellido: ".$empleado1->getApellido();
+        echo "<br>";
+        echo "Sueldo: ".$empleado1->getSueldo();
+        echo "<br>";
+        echo "Sueldo NETO : ".$empleado1->calcularSalarioNeto();
+        echo "<br>";
+        echo "Tarea: ".$empleado1->tarea();
+
+
+        //echo $empleado2;
 
 ?>
+</body>
+</html>
